eecfe5f0c9b88bbc1213880baab8c1f9af1b2591
huutrongdev93 <huutrong.it93@gmail.com>
2024-07-24 11:55:01 +0700

    Chuyển thông tin plugin vào file plugin.json

--- badge-management.php
+++ badge-management.php
@@ -1,12 +1,4 @@
 <?php
-/**
-Plugin name     : Nhãn hiệu sản phẩm
-Plugin class    : badge_management
-Plugin uri      : http://sikido.vn
-Description     : Quản lý Huy hiệu cung cấp cho bạn huy hiệu và khả năng quản lý chúng cho các sản phẩm của cửa hàng của bạn.
-Author          : Nguyễn Hữu Trọng
-Version         : 3.1.0
-*/
 const BADGE_NAME = 'badge-management';
 
 define('BADGE_PATH', Path::plugin(BADGE_NAME));
